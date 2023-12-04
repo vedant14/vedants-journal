369b81db8281e665b13179726d3069d243d2dcc5
Vedant Lohbare <vedant.lohbare@gmail.com>
2023-12-04 16:36:19 +0530

    Update Journal

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ["Ariel", "sans-serif"],
-    },
-    extend: {
-      colors: {
-        "background-white": "#f8f7f6",
-        "brand-white": "#FCFCFC",
-        "brand-blue": "#012F7C",
-        "brand-green": "#48A9A6",
-        "brand-black": "#494953",
-        "brand-red": "#C1666B",
-        "brand-yellow": "#F2BC41",
-      },
-    },
+    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
