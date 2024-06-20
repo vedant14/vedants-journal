637f45c14af766fe1bf0105169f4d576355eea5d
Vedant Lohbare <31346595+vedant14@users.noreply.github.com>
2024-06-20 12:26:53 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import { Layout } from "../components/Layout";
-
+import Head from 'next/head';
 const inter = Inter({ subsets: ["latin"] });
 export default function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+			<link rel='icon' href='/favicon.ico' />
+		</Head>
     <main className={inter.className}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </main>
+    </>
   );
 }
